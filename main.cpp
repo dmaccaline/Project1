e4aadc38abffcccc7fbf87c811d52e0860536d48
dmaccaline <daniel_maccaline@student.uml.edu>
2023-02-11 14:04:45 -0500

    modified to take argument from command line

--- main.cpp
+++ main.cpp
@@ -2,13 +2,13 @@
 
 int main(int argc, char** argv) {
 
+    //get number of children from command line
+    int user_proc = atoi(argv[1]);
+
     pid_t pid = getpid();
     int child_num[25]; //array to hold the child number
     //can expect max. 25 children per the spec (section 3c.)
     int n_processes = 0; //counts number of finished child processes
-    int user_proc; //amount of processes user wants
-
-    cin >> user_proc; //replace with argv input thru the command line
 
 
     cout << "Parent pid is " << pid << endl;
@@ -27,7 +27,7 @@ int main(int argc, char** argv) {
         while(n_processes < user_proc) { //while the number of running processes is less than the amount the user requested
             pid_t child_pid = wait(NULL);
             if(child_pid > 0) { //needed to include this to prevent invalid PIDs of -1? not sure why
-                cout << "Child " << findIndex(child_pid, child_num) << " (PID " << child_pid << ") finished" << endl;
+                cout << "Child " << findIndex(child_pid, child_num)+1 << " (PID " << child_pid << ") finished" << endl;
                 n_processes++; //iterate the number of finished child processes
             }
         }
