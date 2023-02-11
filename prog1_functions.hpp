48308c2fb3b81e0996aee0679fdba2f93e0673e7
dmaccaline <85516223+dmaccaline@users.noreply.github.com>
2023-02-11 13:51:54 -0500

    Create prog1_functions.hpp

---
+++ prog1_functions.hpp
@@ -0,0 +1,23 @@
+//
+//  prog1_functions.hpp
+//  prog_1_os
+//
+//  Created by Jeremy Flotildes on 2/7/23.
+//
+
+#ifndef prog1_functions_hpp
+#define prog1_functions_hpp
+
+#include <stdio.h>
+#include <iostream>
+#include <unistd.h>
+#include <sys/types.h>
+#include <array>
+#include <sys/wait.h>
+
+
+using namespace std;
+
+int findIndex(int pid, int array[25]);
+
+#endif /* prog1_functions_hpp */
